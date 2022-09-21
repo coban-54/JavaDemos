58eef591317b75dee48ce580c3f32b10f52b8d41
coban-54 <92750818+coban-54@users.noreply.github.com>
2022-09-21 16:43:31 +0300

    son commit

---
+++ src/MukemmelSayiBul.java
@@ -0,0 +1,24 @@
+
+public class MukemmelSayiBul {
+
+	public static void main(String[] args) {
+		int number =28;
+		int toplam=0;
+		for(int i=1; i<number;i++)
+		{
+			if(number %i == 0)
+			{
+				toplam =toplam+i;
+			}
+		}
+		if(toplam==number)
+		{
+			System.out.println("mükemmel sayi");
+		}else
+		{
+			System.out.println("mükemmel sayi değildir.");
+		}
+
+	}
+
+}
---
+++ src/SesliHarf.java
@@ -0,0 +1,22 @@
+
+public class SesliHarf {
+
+public static void main(String[] args) {
+		
+		char harf='a';
+		
+		switch(harf)
+		{
+		case 'a':
+		case'ı':
+		case'o':
+		case'u':
+			System.out.println("kalın sesli harf");
+			break;
+		default:
+			System.out.println("ince sesli harf");
+		}
+		
+		
+	}
+}
